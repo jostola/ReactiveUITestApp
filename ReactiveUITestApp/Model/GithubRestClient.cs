ecfbb7732ef7d2b454be250eb5c82efdc6274c61
snarf95 <snarf95@hotmail.com>
2017-06-07 08:44:12 +0200

    Search command is now cancelled when user types something

--- ReactiveUITestApp/Model/GithubRestClient.cs
+++ ReactiveUITestApp/Model/GithubRestClient.cs
@@ -1,4 +1,5 @@
 ﻿using RestSharp;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ReactiveUITestApp.Model {
@@ -6,10 +7,10 @@ namespace ReactiveUITestApp.Model {
         private RestClient Client { get; }
             = new RestClient(@"https://api.github.com/");
 
-        public Task<IRestResponse<RepositoryList>> SearchRepositories(string query) {
+        public Task<IRestResponse<RepositoryList>> SearchRepositories(string query, CancellationToken ct) {
             var request = new RestRequest(@"search/repositories", Method.GET);
             request.AddQueryParameter("q", query);
-            return Client.ExecuteTaskAsync<RepositoryList>(request);
+            return Client.ExecuteTaskAsync<RepositoryList>(request, ct);
         }
     }
 }
--- ReactiveUITestApp/ViewModel/MainWindowViewModel.cs
+++ ReactiveUITestApp/ViewModel/MainWindowViewModel.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reactive;
 using System.Reactive.Linq;
 
 namespace ReactiveUITestApp {
@@ -15,29 +16,36 @@ namespace ReactiveUITestApp {
                 .Select(text => !string.IsNullOrWhiteSpace(text));
 
             this.Search = ReactiveCommand.CreateFromObservable<string, RepositoryList>(
-                 searchTerm => SearchImpl(searchTerm), canSearch);
+                 searchTerm => SearchImpl(searchTerm).TakeUntil(this.CancelCommand), canSearch);
 
-            this.WhenAnyValue(vm => vm.TheText)
-                .Throttle(TimeSpan.FromMilliseconds(500), RxApp.MainThreadScheduler)
-                .InvokeCommand(this, vm => vm.Search);
+            this.CancelCommand = ReactiveCommand.Create(() => { }, this.Search.IsExecuting);
+
+            var textChanges = this.WhenAnyValue(vm => vm.TheText)
+                .Throttle(TimeSpan.FromMilliseconds(500), RxApp.MainThreadScheduler);
+
+            textChanges.InvokeCommand(this, vm => vm.CancelCommand);
+            textChanges.InvokeCommand(this, vm => vm.Search);
 
             this._searchResults =
                 Observable.Merge(
                     this.Search.Select(data => data.items.Select(ConvertToViewModel)),
-                    this.Search.ThrownExceptions.Select(_ => Enumerable.Empty<SearchResultItemViewModel>()),
-                    this.Search.IsExecuting.Where(isExecuting => isExecuting).Select(_ => Enumerable.Empty<SearchResultItemViewModel>()),
-                    canSearch.Where(can => !can).Select(_ => Enumerable.Empty<SearchResultItemViewModel>()))
+                    Observable.Merge(
+                        this.Search.ThrownExceptions.Select(_ => Unit.Default),
+                        this.Search.IsExecuting.Where(isExecuting => isExecuting).Select(_ => Unit.Default),
+                        canSearch.Where(can => !can).Select(_ => Unit.Default))
+                        .Select(_ => Enumerable.Empty<SearchResultItemViewModel>()))
                 .ToProperty(this, vm => vm.SearchResults);
 
             this._searchEventUserInformation =
                 Observable.Merge(
-                    canSearch.DistinctUntilChanged().Select(_canSearch => _canSearch ? (string)null : "Please input something."),
+                    this.WhenAnyValue(vm => vm.TheText).Select(_ => (string)null),
+                    canSearch.DistinctUntilChanged().Where(canExecute => !canExecute).Select(_ => "Please input something."),
                     this.Search.IsExecuting.Where(isExecuting => isExecuting).Select(_ => "Wait one moment."),
                     this.Search.IsExecuting.Where(isExecuting => !isExecuting).PublishLast(
                         _ => Observable.Merge(
                             this.Search.ThrownExceptions.Select(exception => exception.Message),
                             this.WhenAnyValue(vm => vm.SearchResults).Select(results => results != null && results.Any())
-                                .Select(hasResults => hasResults ? (string)null : "No results."))))
+                                .Where(hasResults => !hasResults).Select(__ => "No results."))))
                 .ToProperty(this, vm => vm.SearchEventUserInformation);
         }
 
@@ -45,12 +53,14 @@ namespace ReactiveUITestApp {
             => new SearchResultItemViewModel(repo.name, repo.size.ToString(), repo.score.ToString());
 
         private IObservable<RepositoryList> SearchImpl(string searchTerm)
-            => Observable.FromAsync(() => new GithubRestClient().SearchRepositories(searchTerm)).Select(UnwrapResponse);
+            => Observable.FromAsync(ct => new GithubRestClient().SearchRepositories(searchTerm, ct)).Select(UnwrapResponse);
 
         private static T UnwrapResponse<T>(IRestResponse<T> response)
             => response.StatusCode == HttpStatusCode.OK ? response.Data : throw new Exception($"Bad response: {response.StatusDescription}");
 
         public ReactiveCommand<string, RepositoryList> Search { get; }
+
+        public ReactiveCommand<Unit, Unit> CancelCommand { get; }
 
         private readonly ObservableAsPropertyHelper<IEnumerable<SearchResultItemViewModel>> _searchResults;
         public IEnumerable<SearchResultItemViewModel> SearchResults => _searchResults.Value;
